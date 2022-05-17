f1655777c352ecb76a5ee12bb621cd2d23618c38
fukudaum <fukudaum@gmail.com>
2022-05-16 22:29:41 -0300

    Challenge finalized

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
@@ -6,7 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.json(user);      
+    } catch (error) {
+      return response.status(404).json({error: error.message});
+    }    
   }
 }
 
